5bc12da7f3c322faee3cb3cf650fc5048cbad3eb
JackConceprio1 <jackconceprio1@gmail.com>
2022-07-17 12:37:52 +0100

    Created The File Structure

---
+++ src/screens/create_edit_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CreateEditScreen extends StatelessWidget {
+  const CreateEditScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
